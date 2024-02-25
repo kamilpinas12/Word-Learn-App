feb7e5d12e2ece2f1c6ecde5a25923770d350771
Kamil Pinas <kamil.pinas.pl@gmail.com>
2024-02-25 20:50:03 +0100

    hello world

--- app/__init__.py
+++ app/__init__.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+from flask import Flask
+from config import Config
+
+app = Flask(__name__)
+# app.config.from_object(Config)
+
+
+from app import routes
--- config.py
+++ config.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+
+
