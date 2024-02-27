f38ce549fe7e10cd06b4ca8c4e7b4d6ef21c9ef4
Kamil Pinas <kamil.pinas.pl@gmail.com>
2024-02-27 22:20:23 +0100

    main structure

--- app/models.py
+++ app/models.py
@@ -1,3 +1,22 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import db
+from datetime import datetime
+
+
+class User(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+
+
+class Dataset(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+
+
+class DatasetStats(db.Model):
+    time_stamp: so.Mapped[datetime] = so.mapped_column(sa.String(15))
+    correct: so.Mapped[int] = so.mapped_column()
+    wrong: so.Mapped[int] = so.mapped_column()
+    user_id: so.Mapped[User] = so.mapped_column(sa.ForeignKey(User.id))
